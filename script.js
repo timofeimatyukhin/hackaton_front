bed9b97c728fd6842eb87f534cb1e0b0d8db22c1
timofeimatyukhin <timtim11111111112@gmail.com>
2025-05-18 15:08:00 +0700

    refactor view

--- script.js
+++ script.js
@@ -1,44 +1,7 @@
-const video = document.getElementById('webcam');
-const statusText = document.getElementById('status-text');
+const horDropdown = document.querySelector(".horDropdown");
+const setButton = document.querySelector(".setButton");
+const horDropdown__container = document.querySelector(".horDropdown__container");
 
-// Функция для обновления текста из сервера
-async function updateServerData() {
-    try {
-        
-        statusText.value = 'Here will be placed zalupa from server'
-        
-    } catch (err) {
-        statusText.value = `Ошибка получения данных: ${err.message}`;
-    }
-}
-
-async function setupWebcam() {
-    try {
-        const stream = await navigator.mediaDevices.getUserMedia({ 
-            video: { 
-                width: { ideal: 1280 },
-                height: { ideal: 720 },
-                facingMode: 'user' 
-            }, 
-            audio: false
-        });
-        
-        video.srcObject = stream;
-        
-        // Обновляем данные каждые 3 секунды
-        setInterval(updateServerData, 3000);
-        updateServerData(); // Первоначальный вызов
-        
-    } catch (err) {
-        statusText.value = `Ошибка доступа к камере: ${err.message}`;
-        console.error('Ошибка доступа к камере:', err);
-    }
-}
-
-window.addEventListener('DOMContentLoaded', setupWebcam);
-
-window.addEventListener('beforeunload', () => {
-    if (video.srcObject) {
-        video.srcObject.getTracks().forEach(track => track.stop());
-    }
+setButton.addEventListener("click", () => {
+    horDropdown__container.classList.toggle("horDropdown__container_active");
 });
